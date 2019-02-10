45b9c94b67bfd4ee4777ccbb9f1e3a45eb00fcbf
David Strohmaier <davidstrohmaier92@gmail.com>
2019-02-10 14:19:40 +0000

    forgot to add file

---
+++ src/extractdrs.py
@@ -0,0 +1,72 @@
+import os
+from optparse import OptionParser
+from candc import get_drs
+
+import logging as log
+from lxml import etree
+
+
+log.basicConfig(level=log.INFO)
+
+# command line argument partsing
+parser = OptionParser()
+parser.add_option('-i',
+                  '--input',
+                  dest="input_file",
+                  help="read text from FILE",
+                  metavar="FILE")
+parser.add_option('-d',
+                  '--input-dir',
+                  dest="input_dir",
+                  help="read text files from DIRECTORY",
+                  metavar="DIRECTORY")
+parser.add_option('-o',
+                  '--output',
+                  dest="output_file",
+                  help="write JSON to FILE",
+                  metavar="FILE")
+parser.add_option('-t',
+                  '--tokenized',
+                  action="store_true",
+                  dest="tokenized",
+                  help="do not tokenize input")
+parser.add_option('-c',
+                  '--comentions',
+                  action="store_true",
+                  dest="comentions",
+                  help="output co-mentions")
+(options, args) = parser.parse_args()
+
+
+def create_drs(text):
+    # tokenization
+    if not options.tokenized:
+        log.info("Tokenization")
+        tokens = tokenize(text)
+        if not tokens:
+            log.error("error during tokenization of file '{0}', exiting".format(filename))
+            return None
+        tokenized = "\n".join([' '.join(sentence) for sentence in tokens[:-1]])
+    else:
+        tokenized = text
+
+    #print(postag(tokenized))
+    log.info("Parsing")
+    drs = get_drs(tokenized)
+
+    return drs
+
+if options.input_file:
+    documents = [options.input_file]
+else:
+    documents = [ join(options.input_dir,f) for f in listdir(options.input_dir) if isfile(join(options.input_dir,f)) ]
+
+for filename in documents:
+    # read file
+    log.info("opening file {0}".format(filename))
+    with open(filename) as f:
+        text = f.read()
+        
+
+    drs = create_drs(text)
+    print(drs)
